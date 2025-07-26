3a97f5040a693afd47b985568ccce57c1f4c8584
ВашÐАлександр <veresoki@icloud.com>
2025-07-27 00:22:06 +0300

    last_commit

--- utils/coord_helper.py
+++ utils/coord_helper.py
@@ -3,7 +3,6 @@ from pynput import mouse
 def on_move(x, y):
     print(f'Координаты мыши: X={x}, Y={y}')
 
-# Создаем слушатель мыши
 with mouse.Listener(on_move=on_move) as listener:
     try:
         listener.join()
